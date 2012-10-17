7e907028d304aea5c48d61528f671f4d4cddfc6a
User <user@ASlyusarchuk.(none)>
2012-10-17 18:44:46 +0300

    postgre config changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121016073658) do
+ActiveRecord::Schema.define(:version => 20121016075300) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
